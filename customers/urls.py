f68bf56398de312e3c96425f825fa237d800cf49
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-07-16 14:24:16 +0000

    Add forms and views for user profile

--- customers/urls.py
+++ customers/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserRegisterView, UserEditView, UserProfileView, PasswordsChangeView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+    path('password/', PasswordsChangeView.as_view(template_name='registration/change_password.html')),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
+from .forms import RegisterForm, EditProfileForm, PasswordUpdateForm
+from django.contrib.auth.models import User
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordUpdateForm
+    success_url = reverse_lazy('profile')
 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
+
+
+class UserProfileView(generic.ListView):
+    model = User
+    template_name = 'profile.html'
+    success_url = reverse_lazy('home')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
